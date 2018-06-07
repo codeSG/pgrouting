86fe05a48581a64a35d01df45eaaa91759faf8fa
codeSG <sourabh.8.june.1996@gmail.com>
2018-06-07 12:24:10 +0530

    Fix bellman_ford-typesCheck.sql

--- pgtap/bellman_ford/bellman_ford-typesCheck.sql
+++ pgtap/bellman_ford/bellman_ford-typesCheck.sql
@@ -2,10 +2,17 @@
 SELECT plan(4);
 
 SELECT has_function('pgr_bellman_ford');
+SELECT can(ARRAY['pgr_bellman_ford']);
 
-SELECT has_function('pgr_bellman_ford', ARRAY[ 'text', 'bigint', 'bigint', 'boolean', 'boolean' ]);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','bigint','bigint','boolean', 'boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','bigint','anyarray','boolean', 'boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','anyarray','bigint','boolean', 'boolean']);
+SELECT has_function('pgr_bellman_ford', ARRAY['text','anyarray','anyarray','boolean', 'boolean']);
 
-SELECT function_returns('pgr_bellman_ford', ARRAY[ 'text', 'bigint', 'bigint', 'boolean', 'boolean' ], 'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','bigint','bigint','boolean', 'boolean'],'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','bigint','anyarray','boolean', 'boolean'],'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','anyarray','bigint','boolean', 'boolean'],'setof record');
+SELECT function_returns('pgr_bellman_ford', ARRAY['text','anyarray','anyarray','boolean', 'boolean'],'setof record');
 
 -- testing column names
 SELECT bag_has(
